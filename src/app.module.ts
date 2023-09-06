15e99fca4ecc44e3bbf5440b2449c0f40aa1c47a
Teodor Todorov <teodor.at.tod@gmail.com>
2023-09-06 22:29:55 +0300

    comments and users resourses

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { CommentsModule } from './comments/comments.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule, 
+    CommentsModule,
+    MongooseModule.forRoot('mongodb+srv://teodor:teodor123@cluster0.5xxl6jw.mongodb.net/?retryWrites=true&w=majority')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
